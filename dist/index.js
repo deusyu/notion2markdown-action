84c672db095fe68953a8d07e595a643c840fdbd1
Dorad <ddxi@qq.com>
2023-04-16 00:58:00 +0200

    fix bug when query properties.

--- dist/index.js
+++ dist/index.js
@@ -216862,6 +216862,7 @@ function icon2md(icon) {
 
 function getPropVal(data) {
   let val = data[data.type];
+  if (!val) return '';
   switch (data.type) {
     case "multi_select":
       return val.map((a) => a.name);
