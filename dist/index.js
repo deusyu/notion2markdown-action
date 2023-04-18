2543a768b30f19a65cd6c1d425ca95d2f57a6da3
Dorad <ddxi@qq.com>
2023-04-18 02:16:22 +0200

    change the log type.

--- dist/index.js
+++ dist/index.js
@@ -225335,8 +225335,9 @@ class NotionMigrater extends Migrater.default {
             };
           }
         } catch (e) {
-          console.warn(e);
+          console.warn(`Some error happened when checking image ${url}`);
         }
+        console.log(`Image ${url} not exists`);
         return null;
       }));
       result.urls = result.urls.concat(existsImgs.filter(item => item !== null));
