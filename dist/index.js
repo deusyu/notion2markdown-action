62bbf84bcdec283bcbded0c6e6eac7fc37758407
deusyu <42929363+deusyu@users.noreply.github.com>
2025-08-06 02:16:16 +0800

    build: 更新构建文件到v1.1.12

--- dist/index.js
+++ dist/index.js
@@ -351465,6 +351465,11 @@ async function sync() {
         properties.filename = filename;
     }
     properties.output_dir = path.dirname(properties.filePath);
+    
+    // 🔧 添加页面级别的时间字段用于增量同步
+    properties.last_edited_time = page.last_edited_time;
+    properties.created_time = page.created_time;
+    
     return properties;
   }));
   console.debug(`${notionPagePropList.length} pages found in notion.`);
@@ -382768,7 +382773,7 @@ module.exports = /*#__PURE__*/JSON.parse('["UTF-8","IBM866","ISO-8859-2","ISO-88
 /***/ ((module) => {
 
 "use strict";
-module.exports = /*#__PURE__*/JSON.parse('{"name":"notion2markdown-action","version":"1.1.11","description":"将 Notion 数据库中的页面转换为 Markdown 文档，支持 Hexo、Hugo 等静态博客构建，内置 PicGo-Core 图床上传功能","main":"index.js","scripts":{"test":"node test/test.js","test:video":"node test/video-only.test.js","test:local":"node test/local.test.js","test:incremental":"node test/incremental.test.js","test:config":"node test/config-validator.js","build":"ncc build src/index.js -o dist/"},"author":"deusyu <daniel@deusyu.app>","contributors":["Dorad <https://github.com/Doradx> (Original author)","mohuishou <1@lailin.xyz>"],"license":"MIT","repository":{"type":"git","url":"https://github.com/deusyu/notion2markdown-action.git"},"homepage":"https://github.com/deusyu/notion2markdown-action","bugs":{"url":"https://github.com/deusyu/notion2markdown-action/issues"},"engines":{"node":">=16.0.0"},"keywords":["notion","markdown","hexo","hugo","blog","github-actions","picgo"],"dependencies":{"@actions/core":"^1.10.0","@actions/exec":"^1.1.0","@actions/github":"^5.1.1","@notionhq/client":"^0.4.9","@types/node":"^16.11.12","adm-zip":"^0.5.9","axios":"^0.24.0","cheerio":"^1.0.0-rc.12","dayjs":"^1.10.7","glob":"^7.2.0","image-size":"^1.0.2","imagemin":"^7.0.1","imagemin-gifsicle":"^7.0.0","imagemin-mozjpeg":"^9.0.0","imagemin-pngquant":"^9.0.2","imagemin-svgo":"^9.0.0","install":"^0.13.0","md5-file":"^5.0.0","moment-timezone":"^0.5.43","notion-to-md":"^3.1.1","npm":"^9.6.4","p-queue":"^7.3.4","picgo":"^1.5.6","prettier":"^2.7.1","proxy-agent":"^5.0.0","twemoji":"^13.1.0","yaml":"^2.0.0-9"}}');
+module.exports = /*#__PURE__*/JSON.parse('{"name":"notion2markdown-action","version":"1.1.12","description":"将 Notion 数据库中的页面转换为 Markdown 文档，支持 Hexo、Hugo 等静态博客构建，内置 PicGo-Core 图床上传功能","main":"index.js","scripts":{"test":"node test/test.js","test:video":"node test/video-only.test.js","test:local":"node test/local.test.js","test:incremental":"node test/incremental.test.js","test:config":"node test/config-validator.js","build":"ncc build src/index.js -o dist/"},"author":"deusyu <daniel@deusyu.app>","contributors":["Dorad <https://github.com/Doradx> (Original author)","mohuishou <1@lailin.xyz>"],"license":"MIT","repository":{"type":"git","url":"https://github.com/deusyu/notion2markdown-action.git"},"homepage":"https://github.com/deusyu/notion2markdown-action","bugs":{"url":"https://github.com/deusyu/notion2markdown-action/issues"},"engines":{"node":">=16.0.0"},"keywords":["notion","markdown","hexo","hugo","blog","github-actions","picgo"],"dependencies":{"@actions/core":"^1.10.0","@actions/exec":"^1.1.0","@actions/github":"^5.1.1","@notionhq/client":"^0.4.9","@types/node":"^16.11.12","adm-zip":"^0.5.9","axios":"^0.24.0","cheerio":"^1.0.0-rc.12","dayjs":"^1.10.7","glob":"^7.2.0","image-size":"^1.0.2","imagemin":"^7.0.1","imagemin-gifsicle":"^7.0.0","imagemin-mozjpeg":"^9.0.0","imagemin-pngquant":"^9.0.2","imagemin-svgo":"^9.0.0","install":"^0.13.0","md5-file":"^5.0.0","moment-timezone":"^0.5.43","notion-to-md":"^3.1.1","npm":"^9.6.4","p-queue":"^7.3.4","picgo":"^1.5.6","prettier":"^2.7.1","proxy-agent":"^5.0.0","twemoji":"^13.1.0","yaml":"^2.0.0-9"}}');
 
 /***/ })
 
